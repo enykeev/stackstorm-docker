a11128fc23de4e49f2b46dd20dc14afbc7e9fce4
Kirill Izotov <enykeev@stackstorm.com>
2016-12-08 17:43:12 +0700

    Convert auth dict to literal

--- actions/pull_image.py
+++ actions/pull_image.py
@@ -9,12 +9,12 @@ __all__ = [
 class DockerPullImageAction(DockerBasePythonAction):
     def run(self, repo, tag=None, insecure_registry=False,
             auth_username_override=None, auth_password_override=None):
-        auth_override = (auth_username_override and auth_password_override)
 
-        if auth_override:
-            auth_config = {}
-            auth_config['username'] = auth_username_override
-            auth_config['password'] = auth_password_override
+        if auth_username_override and auth_password_override:
+            auth_config = {
+                'username': auth_username_override,
+                'password': auth_password_override
+            }
             return self.call('pull', repo, tag=tag, insecure_registry=insecure_registry,
                              auth_config=auth_config)
         else:
